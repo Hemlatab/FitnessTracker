c124d10786db9e212cd8bc92c43e53fca03719ca
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-06-05 23:53:28 +0400

    Refactor home page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <head>
 	<title>Fitness & Awareness Web Application</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./styles.css">
 </head>	
 <body>
@@ -27,8 +27,8 @@
 		</nav>
 	</header>
 <div class="home-content">
-<h2>Keep Track with Fitness & Awareness Web Application </h2>
-<h2>The easiest way to keep fit</h2>
+<h1>Keep Track with Fitness & Awareness Web Application </h1>
+<h1>The easiest way to keep fit</h1>
 <br />
 
 <a class="register-btn" href="register.php">Register Today</a>
@@ -37,17 +37,19 @@
 	</div>
 	
 <div class="about">
-<h1 class="home-goals">Reach your goals</h1>
+<h2 class="home-goals">Reach your goals</h2>
 <p>All through the vast majority of human history, the way our    bodies looked was controlled by our battle to survive, not by a cognisant decision to look a specific way. Not any longer. Today, we the way to shape our bodies as we see fit. So, the program is designed to improve your physique and help you build a body that actually worthy of history. Beside, this four week program will make you become more stronger, leaner, and more fit.</p>
-<h1 class="home-goals">Develop healthy diets</h1>
+<h2 class="home-goals">Develop healthy diets</h2>
 <p>All through the vast majority of human history, the way our    bodies looked was controlled by our battle to survive, not by a cognisant decision to look a specific way. Not any longer. Today, we the way to shape our bodies as we see fit. So, the program is designed to improve your physique and help you build a body that actually worthy of history. Beside, this four week program will make you become more stronger, leaner, and more fit. anim id est laborum.</p>
 </div>
 <footer>
 <div class="footer">
-<h2 style="color: darkgrey;">Get Social</h2>
-<a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+<h2>Get Social</h2>
+<div class="social">
+<a href="https://www.facebook.com/"><i class="fa fa-facebook-f"></i></a>
 <a href="https://www.twitter.com/" class="fa fa-twitter"></a>
 <a href="https://www.google.com/" class="fa fa-google"></a>
+</div>
 <p><small>Copyright&copy<?php echo date('Y');?></small></p>
 </div>
 </footer>
